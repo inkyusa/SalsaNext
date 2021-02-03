80c3d4bb0b3e51bedb216105862c91294fee2cbc
Inkyu <inkyu.sa@csiro.au>
2021-02-03 10:20:35 +1000

    model downloading shell script

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+#======================================================
+#  All weights will be stored under this folder
+#======================================================
 export ModelPath=models
 
-PRETRAINED=https://github.com/inkyusa/SalsaNext/releases/download/v0.1/pretrained.zip
-FIRST_TRAINED=https://github.com/inkyusa/SalsaNext/releases/download/v0.1/first_trained_model.zip
+#======================================================
+#  Variables definitions
+#======================================================
+RELEASE_VERSION=v0.1
+GIT_BASE_URL=https://github.com/inkyusa/SalsaNext/releases/download/${RELEASE_VERSION}
+PRETRAINED=pretrained
+FIRST_TRAINED=first_trained
+PRETRAINED_URL=${GIT_BASE_URL}/${PRETRAINED}.zip
+FIRST_TRAINED_URL=${GIT_BASE_URL}/${FIRST_TRAINED}.zip
 
 
-
+#======================================================
+#  Creating a folder to save downloaded models
+#======================================================
 if [ ! -d "${ModelPath}" ]; then
   echo "=================================="
   echo "   Creating ${ModelPath} folder  "
@@ -14,37 +26,43 @@ if [ ! -d "${ModelPath}" ]; then
   mkdir ${ModelPath}
 fi
 
+#======================================================
+#  Downloading pretrained (author provided) weights
+#======================================================
 
-if [ ! -d "${ModelPath}/pretrained" ]; then
-  if [ ! -f "${PRETRAINED" ]; then
+if [ ! -d "${ModelPath}/${PRETRAINED}" ]; then
+  if [ ! -f "${ModelPath}/${PRETRAINED}" ]; then
     echo "=================================="
-    echo "  Downloading ${ROBERTA_BASE} to ${DownloadPath}"
+    echo "  Downloading ${PRETRAINED}.zip to ${ModelPath}/${PRETRAINED}"
     echo "=================================="
-    wget ${ROBERTA_BASE} -P ${DownloadPath}
+    wget ${PRETRAINED_URL} -P ${ModelPath}
   fi
   echo "=================================="
-  echo "  Uncompressing ${ROBERTA_BASE}"
+  echo "  Uncompressing ${PRETRAINED}.zip"
   echo "=================================="
-  unzip -d ${DownloadPath} "${DownloadPath}/${RobertaBase}.zip"
+  unzip -d ${ModelPath} "${ModelPath}/${PRETRAINED}"
   echo "=================================="
-  echo "  Delete ${RobertaBase}.zip"
+  echo "  Delete ${PRETRAINED}.zip"
   echo "=================================="
-  rm "${DownloadPath}/${RobertaBase}.zip"
+  rm "${ModelPath}/${PRETRAINED}.zip"
 fi
 
-if [ ! -d "input/roberta-base" ]; then
-  if [ ! -f "${DownloadPath}/${RobertaBase}.zip" ]; then
+#======================================================
+#  Downloading first trained weights (150 epoches)
+#======================================================
+if [ ! -d "${ModelPath}/${FIRST_TRAINED}" ]; then
+  if [ ! -f "${ModelPath}/${FIRST_TRAINED}" ]; then
     echo "=================================="
-    echo "  Downloading ${ROBERTA_BASE} to ${DownloadPath}"
+    echo "  Downloading ${FIRST_TRAINED}.zip to ${ModelPath}/${FIRST_TRAINED}"
     echo "=================================="
-    wget ${ROBERTA_BASE} -P ${DownloadPath}
+    wget ${FIRST_TRAINED_URL} -P ${ModelPath}
   fi
   echo "=================================="
-  echo "  Uncompressing ${ROBERTA_BASE}"
+  echo "  Uncompressing ${FIRST_TRAINED}.zip"
   echo "=================================="
-  unzip -d ${DownloadPath} "${DownloadPath}/${RobertaBase}.zip"
+  unzip -d ${ModelPath} "${ModelPath}/${FIRST_TRAINED}.zip"
   echo "=================================="
-  echo "  Delete ${RobertaBase}.zip"
+  echo "  Delete ${FIRST_TRAINED}.zip"
   echo "=================================="
-  rm "${DownloadPath}/${RobertaBase}.zip"
+  rm "${ModelPath}/${FIRST_TRAINED}.zip"
 fi
